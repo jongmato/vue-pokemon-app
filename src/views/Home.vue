d470f5d1676d4ff6977ea05cd4dc7d16e43c4830
jonghyun <jonghyeon2046@gmail.com>
2021-02-13 05:14:05 +0900

    finish

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="w-full flex justify-center">
+        <input
+            placeholder="Enter Pokemon here"
+            type="text"
+            class="mt-10 p-2 border-blue-500 border-2"
+            v-model="text"
+        />
+    </div>
+
+    <div class="mt-10 p-4 flex flex-wrap justify-center">
+        <div
+            class="ml-4 text-2xl text-blue-400"
+            v-for="(pokemon, idx) in filteredPokemon"
+            :key="idx"
+        >
+            <router-link :to="`/pokemon/${urlIdLookup[pokemon.name]}`">
+                {{ pokemon.name }}
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive, toRefs, computed } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    setup() {
+        const state = reactive({
+            pokemons: [],
+            filteredPokemon: computed(() => updatePokemon()),
+            text: "",
+            urlIdLookup: {},
+        });
+
+        fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+            .then((res) => res.json())
+            .then((data) => {
+                state.pokemons = data.results;
+                state.urlIdLookup = data.results.reduce(
+                    (a, c, idx) => (a = { ...a, [c.name]: idx + 1 }),
+                    {}
+                );
+                console.log("url", state.urlIdLookup + 1);
+            });
+
+        function updatePokemon() {
+            if (!state.text) {
+                return [];
+            }
+            return state.pokemons.filter((pokemon) =>
+                pokemon.name.includes(state.text)
+            );
+        }
+        return { ...toRefs(state) };
+    },
+};
 </script>
